37a39fd9f8ad749a3e67ef54d0afca6466c0f897
Nelson C <n.devpro@protonmail.com>
2022-11-04 14:08:13 -0400

    dev1.0.6: Testimonials Section

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,27 +1,78 @@
+import profilePic from "../../assets/profile-pic.jpg";
 import Container from "../UIComponents/Container";
 
-const TestimonialsSection = () => {
+type Testimonial = {
+  image: string;
+  alt: string;
+  name: string;
+  job: string;
+  text: string;
+};
+
+const TestimonialCard: React.FC<Testimonial> = ({
+  image,
+  alt,
+  name,
+  job,
+  text,
+}) => {
   return (
-    <section>
+    <div className="border border-gray-200 rounded-md transition duration-200 hover:shadow-xl hover:border hover:border-gray-200">
+      <div className="p-8">
+        <img
+          src={image}
+          alt={alt}
+          className="rounded-full max-w-[90px] mx-auto -mt-20"
+        />
+        <p className="mt-4 text-gray-800 text-[1.1em]">{text}</p>
+      </div>
+      <div className="border-t border-gray-200 p-4 flex flex-col items-center justify-center ">
+        <h3 className="uppercase text-yellow-700 font-semibold">{name}</h3>
+        <p className="text-sm">{job}</p>
+      </div>
+    </div>
+  );
+};
+
+const TestimonialsSection = () => {
+  const testimonials: Testimonial[] = [
+    {
+      image: profilePic,
+      alt: "João Gomes, Vendedor",
+      name: "João Gomes",
+      job: "Vendedor",
+      text: "Nunca tive coragem de falar em público antes. Tudo mudou quando decidi assistir a palestra do Marcos Oliveira. Percebi que toda pessoa tem dentro de si o poder de fazer a diferença através da oratória.",
+    },
+    {
+      image: profilePic,
+      alt: "João Gomes, Vendedor",
+      name: "João Gomes",
+      job: "Vendedor",
+      text: "Nunca tive coragem de falar em público antes. Tudo mudou quando decidi assistir a palestra do Marcos Oliveira. Percebi que toda pessoa tem dentro de si o poder de fazer a diferença através da oratória.",
+    },
+  ];
+
+  return (
+    <section className="py-20">
       <Container>
         <div>
-          {/* Card Slider */}
-          <div>
+          <div className="mb-24 text-center">
+            <h2 className="text-4xl font-medium">
+              O curso que está mudando vidas
+            </h2>
+          </div>
+          {/* Grid */}
+          <div className="grid md:grid-cols-2 gap-x-10 gap-y-20 mx-auto max-w-lg md:max-w-6xl">
             {/* Card */}
-            <div>
-              <div>
-                <p>Image</p>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Recusandae molestiae culpa libero accusantium facere vel
-                  numquam maxime rem placeat cumque?
-                </p>
-              </div>
-              <div>
-                <h3>João Gomes</h3>
-                <p>Vendedor</p>
-              </div>
-            </div>
+            {testimonials.map((testimonial) => (
+              <TestimonialCard
+                image={testimonial.image}
+                alt={testimonial.alt}
+                name={testimonial.name}
+                job={testimonial.job}
+                text={testimonial.text}
+              />
+            ))}
           </div>
 
           <button type="button">Participar</button>
