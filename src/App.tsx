093d75bd213e52e5e2a4fba325df234546f9b4ca
Nelson C <n.devpro@protonmail.com>
2022-11-04 21:43:12 -0400

    dev1.0.9: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import AboutSection from "./components/AboutSection";
 import AdvantagesSection from "./components/Advantages";
@@ -8,8 +7,6 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import SocialProof from "./components/SocialProof";
 import TestimonialsSection from "./components/Testimonials";
-import Title from "./components/Title";
-import Container from "./components/UIComponents/Container";
 
 function App() {
   return (
@@ -22,19 +19,6 @@ function App() {
       <TestimonialsSection />
       <AboutSection />
       <Footer />
-
-      <Container>
-        <div className="flex gap-5 mt-10 justify-center">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </div>
-
-        <h1 className="text-3xl text-center mt-5">Vite + React</h1>
-
-        <div className="text-center">
-          <Title />
-        </div>
-      </Container>
     </div>
   );
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,33 +2,43 @@ import Container from "../UIComponents/Container";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="w-full bg-gray-900 text-slate-50 py-5">
       <Container>
         <div>
           <div>
-            <h3>Marcos Oliveira</h3>
-            <p>
+            <h3 className="text-xl">Marcos Oliveira</h3>
+            <p className="text-base font-light">
               A comunicação não é só falar, é a arte de ver, ouvir e fazer
               entender.
             </p>
           </div>
 
-          <div>
-            <ul>
+          <div className="mt-5">
+            <ul className="flex flex-col gap-2 font-light">
               <li>
-                <a href="/">Home</a>
+                <a href="/" className="hover:underline">
+                  Home
+                </a>
               </li>
               <li>
-                <a href="/">Vantagens</a>
+                <a href="/" className="hover:underline">
+                  Vantagens
+                </a>
               </li>
               <li>
-                <a href="/">Participar</a>
+                <a href="/" className="hover:underline">
+                  Participar
+                </a>
               </li>
               <li>
-                <a href="/">Depoimentos</a>
+                <a href="/" className="hover:underline">
+                  Depoimentos
+                </a>
               </li>
               <li>
-                <a href="/">Sobre</a>
+                <a href="/" className="hover:underline">
+                  Sobre
+                </a>
               </li>
             </ul>
           </div>
