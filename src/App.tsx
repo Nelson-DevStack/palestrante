2089c55fa8d6bfdf2634ec9fb0d893f5af4bb7f3
Nelson C <n.devpro@protonmail.com>
2022-11-05 21:21:04 -0400

    dev1.1.8: folder rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import AboutSection from "./components/AboutSection";
-import AdvantagesSection from "./components/Advantages";
+import AdvantagesSection from "./components/AdvantagesSection";
 import Footer from "./components/Footer";
 import FormSection from "./components/FormSection";
-import Hero from "./components/Hero";
+import Hero from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import SocialProof from "./components/SocialProof";
-import TestimonialsSection from "./components/Testimonials";
+import TestimonialsSection from "./components/TestimonialsSection";
 
 function App() {
   return (
--- src/components/Title/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Title: React.FC = () => {
-  return <h3>Title</h3>;
-};
-
-export default Title;
